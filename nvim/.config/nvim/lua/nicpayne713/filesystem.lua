9899be0054de271804443910c6405a7cc3d5f2b3
Nicholas <nicpayne713@gmail.com>
2021-08-21 20:50:53 -0500

    Clean up of some stuff I don't have yet implemented

--- nvim/.config/nvim/lua/nicpayne713/filesystem.lua
+++ nvim/.config/nvim/lua/nicpayne713/filesystem.lua
@@ -1,7 +1,7 @@
 local map = vim.api.nvim_set_keymap
 local default_opts = {noremap = true}
 
-map('n', '<leader>tt', ':NvimTreeToggle<CR>', {noremap = true, silent = true})
+cap('n', '<leader>tt', ':NvimTreeToggle<CR>', {noremap = true, silent = true})
 map('n', '<leader>fa', ':NvimTreeFindFile<CR>', {noremap = true, silent = true})
 map('n', '<leader>ff', "<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<cr>", default_opts)
 map('n', '<leader>fr', "<cmd>lua require'telescope.builtin'.buffers({ show_all_buffers = true })<cr>", default_opts)
