2558a25bce7d1512cae0b43323dad298dfaca2ea
Nicholas Payne <nic.payne@protonmail.com>
2021-10-19 20:36:03 -0500

    source cmp in lua

--- nvim/.config/nvim/lua/nicpayne713/init.lua
+++ nvim/.config/nvim/lua/nicpayne713/init.lua
@@ -2,6 +2,7 @@ require("nicpayne713.telescope")
 require("nicpayne713.treesitter")
 require("nicpayne713.filesystem")
 require("nicpayne713.lsp-config")
+require("nicpayne713.cmp")
 require("nicpayne713.color")
 
 P = function(v)
