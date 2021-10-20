2af0af233ea4f249f0c28e9f96eb3ea13f0dbd9f
Nicholas Payne <nic.payne@protonmail.com>
2021-10-19 20:35:58 -0500

    Remove a bunch from filesystem.lua

--- nvim/.config/nvim/lua/nicpayne713/filesystem.lua
+++ nvim/.config/nvim/lua/nicpayne713/filesystem.lua
@@ -4,8 +4,3 @@ local default_opts = {noremap = true}
 require'nvim-tree'.setup()
 require'nvim-web-devicons'.setup()
 map('n', '<leader>tt', ':NvimTreeToggle<CR>', {noremap = true, silent = true})
-map('n', '<leader>fa', ':NvimTreeFindFile<CR>', {noremap = true, silent = true})
-map('n', '<leader>ff', "<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '--ignore' }})<cr>", default_opts)
-map('n', '<leader>fr', "<cmd>lua require'telescope.builtin'.buffers({ show_all_buffers = true })<cr>", default_opts)
-map('n', '<leader>fg', "<cmd>lua require'telescope.builtin'.git_status()<cr>", default_opts)
-map('n', '<leader>_', "<cmd>lua require'telescope.builtin'.live_grep()<cr>", default_opts)
