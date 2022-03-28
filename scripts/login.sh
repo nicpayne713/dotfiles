f4e28c2d9e130a8e63dc183555a66bec1d443dca
Nicholas Payne <nic.payne@protonmail.com>
2022-03-28 07:03:28 -0500

    temp ? for cpu temp

--- scripts/login.sh
+++ scripts/login.sh
@@ -3,7 +3,7 @@
 os=`lsb_release -d | awk '{printf "%s %s", $2, $3}'`
 rootFree=`lsblk | awk '{if (length($7)==1) print  $4}'`
 memAvailable=`free -m | grep "Mem" | awk '{printf "%.1fG", $7/1024}'`
-cpuTemp=`sensors | grep CPU | awk '{print $2}' | sed 's/+//' | tr -d '\n'`
+cpuTemp=`echo ?`  #`sensors | grep CPU | awk '{print $2}' | sed 's/+//' | tr -d '\n'`
 loadAvg=`cat /proc/loadavg | awk '{printf "%s %s %s", $1, $2, $3}'`
 
 printf "
