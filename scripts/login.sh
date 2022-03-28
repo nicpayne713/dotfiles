95cca61ac4ba7deb3a5e7a5196a9b6880836d6f9
Nicholas Payne <nic.payne@protonmail.com>
2022-03-27 19:56:46 -0500

    add jess archers login.sh script

---
+++ scripts/login.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+os=`cat /etc/fedora-release | awk '{printf "%s %s", $1, $3}'`
+rootFree=`df -h | grep root | awk '{print $4}' | tr -d '\n'`
+homeFree=`df -h | grep home | awk '{print $4}' | tr -d '\n'`
+memAvailable=`free -m | grep "Mem" | awk '{printf "%.1fG", $7/1024}'`
+cpuTemp=`sensors | grep CPU | awk '{print $2}' | sed 's/+//' | tr -d '\n'`
+loadAvg=`cat /proc/loadavg | awk '{printf "%s %s %s", $1, $2, $3}'`
+
+printf "
+        ⢸⣦⡈⠻⣿⣿⣿⣶⣄          ⣸ ⣯ ⣫ ⣫  ⣵ ⣟ ⣏ ⣿ ⣯ ⣟
+        ⢸⣿⣿⣦⡈⠻⣿⣿⣿⣷⣄
+  ⣀⣀⣀⣀⣀⣀⣼⣿⣿⣿⣿ ⠈⠻⣿⣿⣿⣷⣄        $os
+  ⠈⠻⣿⣿⣿⣿⣿⡿⠿⠛⠁   ⠈⠻⢿⣿⣿⣷⣄     $rootFree   $homeFree   $memAvailable   $cpuTemp  辰$loadAvg
+" | lolcat --truecolor --seed=22 --spread=6
