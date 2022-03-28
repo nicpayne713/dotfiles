28e0e452c0b8338bd5b1c3fe03d0645398b843d2
Nicholas Payne <nic.payne@protonmail.com>
2022-03-27 21:05:49 -0500

    get root free disk space in login script

--- scripts/login.sh
+++ scripts/login.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 os=`lsb_release -d | awk '{printf "%s %s", $2, $3}'`
-rootFree=`df -h | grep root | awk '{print $4}' | tr -d '\n'`
-homeFree=`df -h | grep home | awk '{print $4}' | tr -d '\n'`
+rootFree=`lsblk | awk '{if (length($7)==1) print  $4}'`
 memAvailable=`free -m | grep "Mem" | awk '{printf "%.1fG", $7/1024}'`
 cpuTemp=`sensors | grep CPU | awk '{print $2}' | sed 's/+//' | tr -d '\n'`
 loadAvg=`cat /proc/loadavg | awk '{printf "%s %s %s", $1, $2, $3}'`
@@ -13,7 +12,7 @@ printf "
    _/_/    _/  _/    _/   
   _/  _/  _/  _/_/_/      
  _/    _/_/  _/          漢  $os
-_/      _/  _/            $rootFree   $homeFree   $memAvailable   $cpuTemp  辰$loadAvg
+_/      _/  _/            $rootFree   $memAvailable   $cpuTemp  辰$loadAvg
                          
                          
 " | lolcat --seed=39 --spread=20
