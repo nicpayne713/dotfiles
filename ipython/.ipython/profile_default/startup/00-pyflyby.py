a9ff26e6acc3b0513c253e50db35799be9018833
Nicholas Payne <nic.payne@protonmail.com>
2022-04-02 07:04:55 -0500

    remove commented code

--- ipython/.ipython/profile_default/startup/00-pyflyby.py
+++ ipython/.ipython/profile_default/startup/00-pyflyby.py
@@ -9,20 +9,3 @@ except ImportError:
         stdout=subprocess.DEVNULL,
         stderr=subprocess.DEVNULL,
     ).wait()
-
-# from IPython import get_ipython
-# import subprocess
-
-
-# ipython = get_ipython()
-
-# try:
-#     ipython.magic("load_ext pyflyby")
-# except ModuleNotFoundError:
-#     print("installing pyflyby")
-#     subprocess.Popen(
-#         ["pip", "install", "pyflyby"],
-#         stdout=subprocess.DEVNULL,
-#         stderr=subprocess.DEVNULL,
-#     ).wait()
-#     ipython.magic("load_ext pyflyby")
