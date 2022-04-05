ac53fbf5f79831ad347bbdb44b545bf3cce4145c
Nicholas Payne <nic.payne@protonmail.com>
2022-04-05 08:44:00 -0500

    first pass azpr at work

--- work/.local/bin/azpr.sh
+++ work/.local/bin/azpr.sh
@@ -1,7 +1,20 @@
 #! /bin/bash
 # pick multiple emails from list and combine into comma seperated array
 # emails=`cat ~/dotfiles/work/.caterpillar/emails | fzf -m | sed 's/^\|$/"/g'|paste -sd,` 
-emails=`cat ~/dotfiles/work/.caterpillar/email.examples | fzf -m | sed 's/^\|$/"/g'|paste -sd,` 
 
-echo $emails
+read -p "Pick reviewers (hit enter)"
+emails=`cat ~/dotfiles/work/.caterpillar/email.examples | fzf -m | sed 's/^\|$//g'|paste -sd,` 
+
+read -p "PR Title: " title
+
+read -p "Target Branch: " target
+
+read -p "Squash? (y/n): " squash 
+
+if [ $squash == "y" ]
+then
+    echo "az repos pr create -o table --target-branch $target --squash  --title \"$title\" --reviewers \"$emails\""
+else
+    echo "az repos pr create -o table --target-branch $target --title \"$title\" --reviewers \"$emails\""
+fi
 
