2557176f685876588bcef956f45eefc4945baa7d
Nicholas Payne <nic.payne@protonmail.com>
2022-05-05 10:33:28 -0500

    qutebrowser config

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -6,4 +6,10 @@ config: ConfigAPI = config  # noqa: F821 pylint: disable=E0602,C0103
 c: ConfigContainer = c  # noqa: F821 pylint: disable=E0602,C0103
 
 config.load_autoconfig()
+
+config.bind(
+    "xx",
+    "config-cycle statusbar.show always always;; config-cycle tabs.show always never",
+)
+
 config.source("nord-qutebrowser.py")
