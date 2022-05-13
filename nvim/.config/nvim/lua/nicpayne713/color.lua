e2d34820b5505bbe0edfc2677b48768b343123cb
Nicholas Payne <nic.payne@protonmail.com>
2022-05-13 06:57:28 -0500

    fix comment color

--- nvim/.config/nvim/lua/nicpayne713/color.lua
+++ nvim/.config/nvim/lua/nicpayne713/color.lua
@@ -12,7 +12,7 @@ vim.cmd("colorscheme nord")
 -- Transparency
 vim.cmd[[hi Normal guibg=NONE ctermbg=NONE]]
 vim.cmd[[hi clear Comment]]  -- comments had big blocks like visual selection - turn it off
-vim.cmd[[hi Comment guifg=gray]] -- need to set fg for comments after turning off the weird highlights
+vim.cmd[[hi Comment ctermbg=NONE guifg=gray ctermfg=gray]] -- need to set fg for comments after turning off the weird highlights
 vim.cmd[[hi clear Function]]  -- Had annoying function highlighting in python
 vim.cmd[[hi LineNR guibg=None guifg=gray]]
 vim.cmd[[hi clear SignColumn ]]
