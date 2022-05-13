9e88d1d32a6d15e60c8ec94aefa92131240a4e30
Nicholas Payne <nic.payne@protonmail.com>
2022-05-13 11:49:03 -0500

    fix some highlights

--- nvim/.config/nvim/lua/nicpayne713/color.lua
+++ nvim/.config/nvim/lua/nicpayne713/color.lua
@@ -14,8 +14,11 @@ vim.cmd[[hi Normal guibg=NONE ctermbg=NONE]]
 vim.cmd[[hi clear Comment]]  -- comments had big blocks like visual selection - turn it off
 vim.cmd[[hi Comment ctermbg=NONE guifg=gray ctermfg=gray]] -- need to set fg for comments after turning off the weird highlights
 vim.cmd[[hi clear Function]]  -- Had annoying function highlighting in python
-vim.cmd[[hi LineNR guibg=None guifg=gray]]
+vim.cmd[[hi LineNR guibg=None guifg=gray ctermfg=gray]]
 vim.cmd[[hi clear SignColumn ]]
 vim.cmd[[hi ColorColumn ctermbg=darkgrey guibg=darkgrey]]
-vim.cmd[[hi Folded ctermfg=darkgray]]  -- code fold
+-- vim.cmd[[hi FoldColumn guibg=blue guifg=white ctermbg=red ctermfg=black]]  -- code folds
+vim.cmd[[hi Folded ctermfg=darkgray]]
+
+
 
