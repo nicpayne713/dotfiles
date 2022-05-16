17083c13d97c04daea10230ac19fd1941cb25f06
Nicholas Payne <nic.payne@protonmail.com>
2022-05-16 15:42:51 -0500

    cursorline

--- nvim/.config/nvim/lua/nicpayne713/color.lua
+++ nvim/.config/nvim/lua/nicpayne713/color.lua
@@ -19,6 +19,6 @@ vim.cmd[[hi clear SignColumn ]]
 vim.cmd[[hi ColorColumn ctermbg=darkgrey guibg=darkgrey]]
 -- vim.cmd[[hi FoldColumn guibg=blue guifg=white ctermbg=red ctermfg=black]]  -- code folds
 vim.cmd[[hi Folded ctermfg=darkgray]]
-
-
+vim.cmd[[hi clear CursorLine]]
+vim.cmd[[hi CursorLine guifg=none guibg=black ]]
 
