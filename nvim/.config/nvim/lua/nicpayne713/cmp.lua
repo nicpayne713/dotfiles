259774184c6782138fd198f539782af976d1be25
Nicholas Payne <nic.payne@protonmail.com>
2022-05-17 14:48:09 -0500

    I think I added spell and tab9

--- nvim/.config/nvim/lua/nicpayne713/cmp.lua
+++ nvim/.config/nvim/lua/nicpayne713/cmp.lua
@@ -41,13 +41,42 @@ cmp.setup({
     { name = 'buffer' },
     { name = 'path' },
     { name = 'tmux' },
-    })
+    { name = 'spell' },
+    { name = 'cmp_tabnine' }
+    }),
+    formatting = {
+            format = function(entry, vim_item)
+                vim_item.kind = lspkind.presets.default[vim_item.kind]
+                local menu = source_mapping[entry.source.name]
+                if entry.source.name == 'cmp_tabnine' then
+                    if entry.completion_item.data ~= nil and entry.completion_item.data.detail ~= nil then
+                        menu = entry.completion_item.data.detail .. ' ' .. menu
+                    end
+                    vim_item.kind = ''
+                end
+                vim_item.menu = menu
+                return vim_item
+            end
+        },
 })
 
 local lspkind = require('lspkind')
+
+local source_mapping = {
+    buffer = "[Buffer]",
+    nvim_lsp = "[LSP]",
+    nvim_lua = "[Lua]",
+    cmp_tabnine = "[TN]",
+    path = "[Path]",
+}
+
 cmp.setup {
   formatting = {
     format = lspkind.cmp_format({with_text = false, maxwidth = 50})
   }
 }
 
+
+-- spelling
+vim.g.opt_spell = true
+vim.g.opt_spelllang = { 'en_us' }
